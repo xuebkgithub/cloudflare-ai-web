414410fc2b95d71a679a5a942b218b17f07155a6
熊猫 <41730586+xuebkgithub@users.noreply.github.com>
2023-12-29 19:21:30 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,11 @@ export default defineNuxtConfig({
             prerender: true,
         }
     },
+    nitro: {
+     vercel: {
+         regions: ["cle1", "iad1", "pdx1", "sfo1", "sin1", "syd1", "hnd1", "kix1"]
+     }
+    },
     app: {
         head: {
             title: 'CF AI Web',
